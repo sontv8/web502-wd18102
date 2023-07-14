abbfef15e2c5d89b07c90bd8136b93f6a7347837
sontv8 <sontv8@fpt.edu.vn>
2023-07-14 10:44:56 +0700

    day4-components

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div>
+                <img src="https://picsum.photos/900/300" alt="" />
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Product</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
