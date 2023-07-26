71956e8c9045f0359133c495bae0765056b6d389
sontv8 <sontv8@fpt.edu.vn>
2023-07-26 10:38:54 +0700

    day9 - add product with multiple input

--- my-app/src/pages/Admin/AddProduct.jsx
+++ my-app/src/pages/Admin/AddProduct.jsx
@@ -2,19 +2,22 @@ import React from 'react'
 import { useState } from 'react';
 
 const AddProductPage = ({ addProduct }) => {
-    const [data, setData] = useState({})
+    const [data, setData] = useState({}) // tạo state để lưu giá trị của input
     const onHandleChange = (event) => {
-        setData({ "name": event.target.value });
+        const { name, value } = event.target; // lấy name và value của input
+        const newData = { ...data, [name]: value } // lấy tất cả các giá trị cũ và thêm giá trị mới
+        setData(newData); // cập nhật lại state
     }
     const onHandleSubmit = (e) => {
         e.preventDefault();
-        addProduct(data);
+        addProduct(data); // gọi hàm addProduct được truyền từ component cha
     }
 
     return (
         <div>
             <form action="" onSubmit={onHandleSubmit}>
-                <input type="text" placeholder='Enter Product Name' onChange={onHandleChange} />
+                <input type="text" placeholder='Enter Product Name' onChange={onHandleChange} name='name' />
+                <input type="text" placeholder='Enter Product Price' onChange={onHandleChange} name='price' />
                 <button>Add New</button>
             </form>
         </div>
